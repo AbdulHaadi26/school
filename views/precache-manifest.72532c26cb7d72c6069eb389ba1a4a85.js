e507dc94d27892b478d3ec51c1a2c8ff7862a77f
AbdulHaadi26 <abdulhaadirao@gmail.com>
2020-03-28 00:17:21 +0500

    initailize front end

---
+++ views/precache-manifest.72532c26cb7d72c6069eb389ba1a4a85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "136a21da1d00f4a003096f9695ca0087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91c09e388d0cc76f09fa",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "05ac65b61bcec742b322",
+    "url": "/static/css/main.43ae3f50.chunk.css"
+  },
+  {
+    "revision": "91c09e388d0cc76f09fa",
+    "url": "/static/js/2.441fa16f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.441fa16f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ac65b61bcec742b322",
+    "url": "/static/js/main.75ebb7cb.chunk.js"
+  },
+  {
+    "revision": "e24f1fa857b4ae46f8ca",
+    "url": "/static/js/runtime-main.21995e34.js"
+  }
+]);
