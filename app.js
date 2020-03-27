2af6beff1d7eec92baa3a73fd862813919d45110
AbdulHaadi26 <abdulhaadirao@gmail.com>
2020-03-28 01:12:25 +0500

    initailize front end

--- app.js
+++ app.js
@@ -15,24 +15,24 @@ const mongoURI = `mongodb+srv://${process.env.NAME}:${process.env.PASS}@dev-02rq
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/build', express.static(path.join(__dirname, 'build')));
+app.use('/public',express.static('public'));
+app.use(express.static('build'));
 
 //Routes
 require('./routes')(app);
 
 //Send an HTML page to confirm that the website is working
 app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, 'build/index.html'));;
+    res.sendFile(__dirname + '/build/index.html');
 });;
 
 //Send an HTML page to confirm that the website is working
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build/index.html'));
+res.sendFile(__dirname + '/build/index.html');
 });
 
 //Connect to Mongodb
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true}, function (err) {
     if (err)
         console.log(err)
     else
@@ -41,6 +41,6 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, 
 
 //Start the server on port
 app.listen(process.env.PORT, () => {
-    console.log('Server is listening on port ' + process.env.Port);
+    console.log('Server is listening on port '+process.env.Port);
 });
 
