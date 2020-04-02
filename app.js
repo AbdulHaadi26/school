c9231884204e552b66c6bccd0a888ca0370b138b
AbdulHaadi26 <abdulhaadirao@gmail.com>
2020-04-02 23:11:35 +0500

    new page

--- app.js
+++ app.js
@@ -50,14 +50,14 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, 
     ;
 
 //Start the server on port
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
     var user = User.find({});
-    Promise.all(user.map(async (user) => {
+    await Promise.all(user.map(async (user) => {
         User.updateOne({ '_id': mongoose.Types.ObjectId(user._id) }, { $set: { cls: Number(user.cls), roll: Number(user.roll) } })
     }));
 
     var result = Result.find({});
-    Promise.all(result.map(async (result => {
+    await Promise.all(result.map(async (result => {
         Result.updateOne(
             { '_id': mongoose.Types.ObjectId(result._id) }, 
         { $set: { cls: Number(result.cls), roll: Number(result.roll) } });
